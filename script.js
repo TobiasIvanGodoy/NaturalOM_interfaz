ebc425682ace4628031428b306e7d9e683531fe1
Tobias Ivan Godoy <tobiasig2001@gmail.com>
2026-07-12 22:28:39 -0300

    modularice eventos repetidos

--- script.js
+++ script.js
@@ -10,12 +10,13 @@ const tablaMovimientos = document.getElementById("tablaMovimientos")
 
 // Otros
 
-
 const saldo = document.getElementById("saldo")
 const informacion = document.getElementById("informacion")
 const overlay = document.getElementById("overlay")
 const contenedor = document.getElementById("contenedor")
 const cerrar = document.getElementById("cerrar")
+
+// Funciones 
 
 function mostrarMensaje(texto, tipo) {
     informacion.textContent = texto;
@@ -29,107 +30,96 @@ function cerrarMenu(elementos) {
         j.remove();
     }
 }
-btnProducto.addEventListener(
-    "click",
-    function () {
-        const elementos = [];
 
-        overlay.classList.remove("oculto");
-        const lista = ["producto", "precio", "stock"];
-        for (const i of lista) {
-            const label = document.createElement("label");
-            label.htmlFor = i;
-            label.textContent = i;
+function abrirMenu(diccionario) {
+    const elementos = [];
+    for (const i in diccionario) {
+        const label = document.createElement("label");
+        label.htmlFor = i;
+        label.textContent = i;
 
-            const input = document.createElement("input");
-            input.id = i;
-            input.type = (i === "precio" || i === "stock") ? "number" : "text";
-            input.placeholder = `Ingrese el ${i}...`
+        const input = document.createElement("input");
+        input.id = i;
+        input.type = diccionario[i][1];
+        input.placeholder = diccionario[i][0];
+
+        contenedor.appendChild(label);
+        contenedor.appendChild(input);
+
+        elementos.push(label);
+        elementos.push(input);
+    }
+    return elementos;
+}
+
+function crearBoton(id, texto, cont) {
+    const boton = document.createElement("button");
+    boton.id = id;
+    boton.textContent = texto;
+    cont.appendChild(boton);
+    return boton
+}
+
+const botonesAgregados = [
+    {
+    boton : btnProducto,
+    campos : 
+        {"producto" : ["Ingrese el nombre del producto", "text"],
+            "precio" : ["ingrese el precio del producto", "number"], 
+            "stock" : ["Ingrese el stock actual", "number"]
+        }
+    },
+    {
+    boton : btnMovimiento,
+    campos :
+        {"mensaje" : ["Describí el movimiento", "text"],
+        "precio" : ["Ingrese el monto del movimiento", "number"]
+        }
+    }
+]
+
+// eventos
+
+for (const configuracion of botonesAgregados) {
+    configuracion.boton.addEventListener(
+        "click",
+        function () {
+            overlay.classList.remove("oculto");
             
-            contenedor.appendChild(label);
-            contenedor.appendChild(input);
-
-            elementos.push(label);
-            elementos.push(input);
+            const elementos = abrirMenu(configuracion.campos)
+            
+            if (configuracion.boton === btnMovimiento) {
+                const btnGasto = crearBoton("btnGastoNoPress", "Gasto", contenedor)
+                elementos.push(btnGasto)
+                
+                btnGasto.addEventListener(
+                    "click",
+                    function () {
+                        //más lógica del backend
+                    }
+                )
             }
-        
-        const btnConfirmar = document.createElement("button");
-        btnConfirmar.id = "btnConfirmar";
-        btnConfirmar.textContent = "confirmar";
-        contenedor.appendChild(btnConfirmar);
-        elementos.push(btnConfirmar)
 
-        const btnCerrar = document.createElement("button");
-        btnCerrar.id = "btnCerrar";
-        btnCerrar.textContent = "X";
-        cerrar.appendChild(btnCerrar);
-        elementos.push(btnCerrar);
+            const btnConfirmar = crearBoton("btnConfirmar", "confirmar", contenedor);
+            elementos.push(btnConfirmar)
 
-        btnCerrar.addEventListener(
-            "click",
-            function () {
-            cerrarMenu(elementos)
-            }
-        )
+            const btnCerrar = crearBoton("btnCerrar", "X", cerrar);
+            elementos.push(btnCerrar);
 
-        btnConfirmar.addEventListener(
-            "click",
-            function() {
-            //aca va a ir la lógica del backend
-            cerrarMenu(elementos);
-            }
-        )
-    } 
-)
+            btnCerrar.addEventListener(
+                "click",
+                function () {
+                cerrarMenu(elementos)
+                }
+            )
 
-btnMovimiento.addEventListener(
-    "click",
-    function () {
-        const elementos = [];
-
-        overlay.classList.remove("oculto");
-        const lista = ["mensaje", "monto"];
-        for (const i of lista) {
-            const label = document.createElement("label");
-            label.htmlFor = i;
-            label.textContent = i;
-
-            const input = document.createElement("input");
-            input.id = i;
-            input.type = (i === "monto") ? "number" : "text";
-            input.placeholder = `Ingrese el ${i} del movimiento...`
-
-            contenedor.appendChild(label);
-            contenedor.appendChild(input);
-
-            elementos.push(label);
-            elementos.push(input);
-            }
-        
-        const btnConfirmar = document.createElement("button");
-        btnConfirmar.id = "btnConfirmar";
-        btnConfirmar.textContent = "confirmar";
-        contenedor.appendChild(btnConfirmar);
-        elementos.push(btnConfirmar)
-
-        const btnCerrar = document.createElement("button");
-        btnCerrar.id = "btnCerrar";
-        btnCerrar.textContent = "X";
-        cerrar.appendChild(btnCerrar);
-
-        btnCerrar.addEventListener(
-            "click",
-            function () {
-            cerrarMenu(elementos)
-            }
-        )
-        
-        btnConfirmar.addEventListener(
-            "click",
-            function() {
-            //aca va a ir la lógica del backend
-            cerrarMenu(elementos);
-            }
-        )
-    } 
-)
+            btnConfirmar.addEventListener(
+                "click",
+                function() {
+                //aca va a ir la lógica del backend
+                cerrarMenu(elementos);
+                }
+            )
+        }
+    )
+}
