973d41f613f4e206d3e1a3b307c4196de5b2a447
Tobias Ivan Godoy <tobiasig2001@gmail.com>
2026-07-16 01:40:17 -0300

    Agregue todos los formularios

--- script.js
+++ script.js
@@ -20,68 +20,311 @@ const overlay = document.getElementById("overlay");
 // FUNCIONES
 // ==========================================================================
 
-function cerrarMenu(elementos) {
-    overlay.classList.add("oculto");
 
-    for (const elemento of elementos) {
-        elemento.remove();
-    }
+function crearCampo(entrada, contenedor) {
+    const campo = document.createElement("div");
+    campo.className = "campo";
+
+    const label = document.createElement("label");
+    label.htmlFor = entrada.id;
+    label.textContent = entrada.id;
+
+    const input = document.createElement("input");
+    input.id = entrada.id;
+    input.type = entrada.tipo;
+    input.placeholder = entrada.placeholder;
+
+    campo.append(label, input);
+    contenedor.appendChild(campo);
 }
 
-function crearCampo(id, placeholder, tipo) {
+function nuevoProducto() {
+
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("contenedorMenu");
+    overlay.appendChild(contenedor);
+
+    const cerrar = document.createElement("div");
+    cerrar.classList.add("contenedorRow");
+    cerrar.style.justifyContent = "flex-end";
+    cerrar.style.width = "100%";
+    contenedor.appendChild(cerrar);
+
+    const btnCerrar = document.createElement("button");
+    btnCerrar.id = "btnCerrar"
+    btnCerrar.textContent = "X"
+    cerrar.appendChild(btnCerrar)
+    btnCerrar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+    })
+
+    const inputs = [
+            {id : "producto",
+            tipo : "text",
+            placeholder : "Ingrese el nombre del producto..."
+    },
+            {id: "precio",
+            tipo: "number",
+            placeholder : "Ingrese el precio..."
+            },
+            {id: "cantidad",
+            tipo: "number",
+            placeholder : "Ingrese el stock incial..."}
+            ]
+
+    for (const i of inputs) {
+        crearCampo(i, contenedor);
+    }
 
     const campo = document.createElement("div");
     campo.className = "campo";
 
     const label = document.createElement("label");
-    label.htmlFor = id;
-    label.textContent = id;
+    label.htmlFor = "distribuidores";
+    label.textContent = "distribuidor";
 
-    const input = document.createElement("input");
-    input.id = id;
-    input.type = tipo;
-    input.placeholder = placeholder;
+    const select = document.createElement("select");
+    select.name = "distribuidores";
+    select.style.width = "100%";
+    select.id = "distribuidores";
 
-    campo.append(label, input);
-    contenedor.appendChild(campo);
+    const ph = document.createElement("option");
+    ph.textContent = "distribuidores agendados...";
+    ph.value = "";
+    ph.disabled = true;
+    ph.selected = true;
 
-    return campo;
+    select.append(ph);
+
+
+    //lógica del backend para buscar los distribuidores registrados
+
+    campo.append(label, select);
+    contenedor.appendChild(campo)
+
+    const btnConfirmar = document.createElement("button");
+    btnConfirmar.classList.add("btnAgregar")
+    btnConfirmar.textContent = "Confirmar"
+    contenedor.appendChild(btnConfirmar)
+    btnConfirmar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+        //logica de guardado en el backend
+    })
+
 }
 
-function abrirMenu(diccionario) {
 
-    const elementos = [];
+function reponerStock() {
+    
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("contenedorMenu");
+    overlay.appendChild(contenedor);
 
-    for (const id in diccionario) {
+    const cerrar = document.createElement("div");
+    cerrar.classList.add("contenedorRow");
+    cerrar.style.justifyContent = "flex-end";
+    cerrar.style.width = "100%";
+    contenedor.appendChild(cerrar);
 
-        const placeholder = diccionario[id][0];
-        const tipo = diccionario[id][1];
+    const btnCerrar = document.createElement("button");
+    btnCerrar.id = "btnCerrar"
+    btnCerrar.textContent = "X"
+    cerrar.appendChild(btnCerrar)
+    btnCerrar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+    })
 
-        const campo = crearCampo(id, placeholder, tipo);
+    const campo = document.createElement("div");
+    campo.className = "campo";
 
-        elementos.push(campo);
+    const label = document.createElement("label");
+    label.htmlFor = "producto";
+    label.textContent = "producto";
+
+    const select = document.createElement("select");
+    select.name = "producto";
+    select.style.width = "100%";
+    select.id = "producto";
+
+    const ph = document.createElement("option");
+    ph.textContent = "productos guardados...";
+    ph.value = "";
+    ph.disabled = true;
+    ph.selected = true;
+
+    select.append(ph);
+
+    //lógica del backend para buscar los productos guardados
+
+    campo.append(label, select);
+    contenedor.appendChild(campo)
+
+    const inputs = [
+            {id : "cantidad",
+            tipo : "number",
+            placeholder : "Cantidad de stock repuesto..."
+    },
+            {id: "monto",
+            tipo: "number",
+            placeholder : "Costo de la reposición..."
+            }
+            ]
+
+    for (const i of inputs) {
+        crearCampo(i, contenedor);
     }
 
-    return elementos;
+    const btnConfirmar = document.createElement("button");
+    btnConfirmar.classList.add("btnAgregar")
+    btnConfirmar.textContent = "Confirmar"
+    contenedor.appendChild(btnConfirmar)
+    btnConfirmar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+        //logica de guardado en el backend
+    })
+
 }
 
-function crearBoton(id, texto, contenedor, clases = []) {
-
-    const boton = document.createElement("button");
-
-    boton.id = id;
-    boton.textContent = texto;
-
-    boton.classList.add(...clases);
-
-    contenedor.appendChild(boton);
-
-    return boton;
-}
-
-function modificarOverlay() {
+function añadirGasto() {
     
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("contenedorMenu");
+    overlay.appendChild(contenedor);
+
+    const cerrar = document.createElement("div");
+    cerrar.classList.add("contenedorRow");
+    cerrar.style.justifyContent = "flex-end";
+    cerrar.style.width = "100%";
+    contenedor.appendChild(cerrar);
+
+    const btnCerrar = document.createElement("button");
+    btnCerrar.id = "btnCerrar"
+    btnCerrar.textContent = "X"
+    cerrar.appendChild(btnCerrar)
+    btnCerrar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+    })
+
+    const campo = document.createElement("div");
+    campo.className = "campo";
+
+    const label = document.createElement("label");
+    label.htmlFor = "categoria";
+    label.textContent = "categoria";
+
+    const select = document.createElement("select");
+    select.name = "categoria";
+    select.style.width = "100%";
+    select.id = "categoria";
+
+    const ph = document.createElement("option");
+    ph.textContent = "categorias registradas...";
+    ph.value = "";
+    ph.disabled = true;
+    ph.selected = true;
+
+    select.append(ph);
+
+    const nuevo = document.createElement("option");
+    nuevo.textContent = "...";
+    nuevo.value = "...";
+
+    select.append(nuevo);
+
+    //lógica del backend para buscar las categorias registradas
+
+    campo.append(label, select);
+    contenedor.appendChild(campo)
+
+    const inputs = [
+            {id: "monto",
+            tipo: "number",
+            placeholder : "Monto del gasto..."
+            }
+            ]
+
+    for (const i of inputs) {
+        crearCampo(i, contenedor);
+    }
+
+    const btnConfirmar = document.createElement("button");
+    btnConfirmar.classList.add("btnAgregar")
+    btnConfirmar.textContent = "Confirmar"
+    contenedor.appendChild(btnConfirmar)
+    btnConfirmar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+        //logica de guardado en el backend
+    })
+
 }
+
+function agregarDistribuidor() {
+    
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("contenedorMenu");
+    overlay.appendChild(contenedor);
+
+    const cerrar = document.createElement("div");
+    cerrar.classList.add("contenedorRow");
+    cerrar.style.justifyContent = "flex-end";
+    cerrar.style.width = "100%";
+    contenedor.appendChild(cerrar);
+
+    const btnCerrar = document.createElement("button");
+    btnCerrar.id = "btnCerrar"
+    btnCerrar.textContent = "X"
+    cerrar.appendChild(btnCerrar)
+    btnCerrar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+    })
+
+    const inputs = [
+            {id: "nombre",
+            tipo: "text",
+            placeholder : "Nombre del distribuidor..."
+            },
+            {id: "dirección",
+            tipo: "text",
+            placeholder : "Dirección del distribuidor..."
+            },
+            {id: "página web",
+            tipo: "text",
+            placeholder : "Página web del distribuidor..."
+            }
+            ]
+
+    for (const i of inputs) {
+        crearCampo(i, contenedor);
+    }
+
+    const btnConfirmar = document.createElement("button");
+    btnConfirmar.classList.add("btnAgregar")
+    btnConfirmar.textContent = "Confirmar"
+    contenedor.appendChild(btnConfirmar)
+    btnConfirmar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+        //logica de guardado en el backend
+    })
+
+}
+
+function crearGraficos() {
+    //solo un placeholder para tener algo.
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("contenedorMenu");
+    overlay.appendChild(contenedor);
+    
+    const btnConfirmar = document.createElement("button");
+    btnConfirmar.classList.add("btnAgregar")
+    btnConfirmar.textContent = "Confirmar"
+    contenedor.appendChild(btnConfirmar)
+    btnConfirmar.addEventListener("click", function (){
+        overlay.classList.add("oculto")
+    })
+
+
+}
+
 // ==========================================================================
 // CONFIGURACIÓN
 // ==========================================================================
@@ -97,27 +340,31 @@ const botonesAgregados = [
 
         id: "tablaProductos",
 
-        campos: []
+        modificarOverlay : nuevoProducto
     },
 
     {
         boton: btnMovStock,
 
-        atributos: ["Categoria", "Producto", "Cantidad", "Monto", "Fecha"],
+        atributos: ["Categoria", "Producto", "Cantidad", "Monto", "Fecha", "Hora"],
 
         agregar: "Reponer stock",
 
-        id: "tablaMovStock"
+        id: "tablaMovStock",
+
+        modificarOverlay : reponerStock
     },
 
     {
         boton: btnGastos,
 
-        atributos: ["Categoria", "Monto", "Fecha"],
+        atributos: ["Categoria", "Monto", "Fecha", "Hora"],
 
         agregar: "Añadir gasto",
 
-        id: "tablaGastos"
+        id: "tablaGastos",
+
+        modificarOverlay : añadirGasto
     },
 
     {
@@ -127,7 +374,9 @@ const botonesAgregados = [
 
         agregar: "Agregar distribuidor",
 
-        id: "tablaDistribuidores"
+        id: "tablaDistribuidores",
+
+        modificarOverlay : agregarDistribuidor
     },
 
     {
@@ -137,7 +386,9 @@ const botonesAgregados = [
 
         agregar: "En construcción",
 
-        id: "tablaEstadisticas"
+        id: "tablaEstadisticas",
+
+        modificarOverlay : crearGraficos
     }
 ];
 
@@ -168,10 +419,12 @@ for (const configuracion of botonesAgregados) {
 
         const boton = document.createElement("button");
         boton.textContent = configuracion.agregar;
-        boton.id = "btnAgregar";
+        boton.classList.add("btnAgregar");
 
         boton.addEventListener("click", function(){
-            modificarOverlay();
+            overlay.innerHTML = "";
+            overlay.classList.remove("oculto");
+            configuracion.modificarOverlay();
         })
         seccionActual.appendChild(boton);
     })
